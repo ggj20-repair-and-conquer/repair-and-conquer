5597ce83e059c6e9b40dbf4055f05a73e02bcfb6
jas <jas@tradebyte.biz>
2020-01-31 23:12:21 +0100

    Added logo and button and fixed body alignment

--- src/scenes/MenuScene.js
+++ src/scenes/MenuScene.js
@@ -1,14 +1,27 @@
 import 'phaser';
+import config from '../config/config.js';
+
 
 export default class MenuScene extends Phaser.Scene {
     constructor() {
         super('Menu');
     }
 
-    preload() {
+   preload() {
+        this.load.image('background', 'assets/buttons/background.png');
+        this.load.image('btn_create', 'assets/buttons/btn_create.png');
+        this.load.image('btn_join', 'assets/buttons/btn_join.png');
+        this.load.image('logo', 'assets/logo.png');
     }
 
     create() {
+
+        const background = this.add.image(config.width/2, config.height/2, 'background');
+        background.setDisplaySize(config.width, config.height);
+
+        const logo = this.add.image(config.width/2, config.height/2, 'logo');
+        logo.setInteractive();
+
         const btnCreateGame = this.add.image(400, 750, 'btn_create');
         btnCreateGame.setInteractive();
         btnCreateGame.on('pointerdown', () => {
