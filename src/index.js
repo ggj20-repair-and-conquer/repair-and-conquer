8134a1605d710a17e727b2a0dc29a94c45311203
Dominik Meyer <dom@tradebyte.biz>
2020-01-31 20:04:51 +0100

    adding menu scene

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import 'phaser';
 import config from './config/config';
 import BootScene from './scenes/BootScene';
-
+import MenuScene from './scenes/MenuScene';
 
 class Game extends Phaser.Game {
     constructor () {
         super(config);
         this.scene.add('Boot', BootScene);
+        this.scene.add('Menu', MenuScene);
         this.scene.start('Boot');
     }
 
