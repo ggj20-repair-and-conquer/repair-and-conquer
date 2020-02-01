b4066e370d31172cf9deb769f12704a1933d2968
jasbir <esspal7725@gmail.com>
2020-02-01 12:12:34 +0100

    Create game scene: Fixed alignments & added validations

--- src/scenes/CreateGameScene.js
+++ src/scenes/CreateGameScene.js
@@ -45,16 +45,19 @@ export default class CreateGameScene extends Phaser.Scene {
         /*
          * game-name
          */
-        const gameText = this.add.text(700, 100, 'Game room name', {
-            font: '32px Courier',
-            fill: '#ffffff'
+        const gameText = this.add.text(640, 150, 'Game room name', {
+            font: '48px Serif',
+            fill: '#ffffff',
+            strokeThickness: 6,
+            stroke: '#000',
+            fontWeight: 'bold'
         });
         gameText.setInteractive();
         gameText.on('pointerdown', () => {
             this.keyFocus = gameRoomText;
         }, this);
 
-        const gameBackground = this.add.image(820, 200, 'txt_background');
+        const gameBackground = this.add.image(820, 250, 'txt_background');
         gameBackground.setInteractive();
         gameBackground.on('pointerdown', () => {
             this.keyFocus = gameRoomText;
@@ -63,16 +66,19 @@ export default class CreateGameScene extends Phaser.Scene {
         /*
          * player-name
          */
-        const playerText = this.add.text(700, 300, 'Player name', {
-            font: '32px Courier',
-            fill: '#ffffff'
+        const playerText = this.add.text(700, 350, 'Player name', {
+            font: '48px Serif',
+            fill: '#ffffff',
+            strokeThickness: 6,
+            stroke: '#000',
+            fontWeight: 'bold'
         });
         playerText.setInteractive();
         playerText.on('pointerdown', () => {
             this.keyFocus = playerNameText;
         }, this);
 
-        const playerBackground = this.add.image(820, 400, 'txt_background')
+        const playerBackground = this.add.image(820, 450, 'txt_background')
         playerBackground.setInteractive();
         playerBackground.on('pointerdown', () => {
             this.keyFocus = playerNameText;
@@ -95,18 +101,28 @@ export default class CreateGameScene extends Phaser.Scene {
         btnCreate.setInteractive();
         btnCreate.on('pointerdown', () => {
             clickSound.play();
+            if (gameRoomText.text == '' || playerNameText.text == '') {
+            	const errorText = this.add.text(700, 550, 'Please fill the inputs', {
+		            font: '32px Serif',
+		            fill: 'red',
+		            strokeThickness: 6,
+		            stroke: '#000',
+		            fontWeight: 'bold'
+		        });
+            }
             this.createGame(gameRoomText, playerNameText);
         }, this);
 
-        let gameRoomText = this.add.text(590, 180, '', {
+        let gameRoomText = this.add.text(590, 230, '', {
             font: '32px Courier',
             fill: '#fff',
             strokeThickness: 6,
             stroke: '#000',
-            fontWeight: 'bold'
+            fontWeight: 'bold',
+            wordWrap: true
         });
 
-        let playerNameText = this.add.text(590, 380, '', {
+        let playerNameText = this.add.text(590, 430, '', {
             font: '32px Courier',
             fill: '#fff',
             strokeThickness: 6,
