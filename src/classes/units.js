8bcf8edf631a6fa81d22998e2e9ee12114a6ef2f
Dominik Meyer <dom@tradebyte.biz>
2020-02-02 15:16:56 +0100

    was?

--- src/classes/units.js
+++ src/classes/units.js
@@ -159,7 +159,7 @@ export class Unit extends Phaser.Physics.Arcade.Sprite {
                     type: 'attackBuilding',
                     gameId: socket.gameData.gameId,
                     buildingId: this.attackTarget.buildingId,
-                    unitType: this.unitType
+                    unitType: this.unitType,
                 });
                 this.currentCooldown += this.cooldown;
             }
